2448ad301360156b3a25f77f44ac6fbd5dc254a8
Obi <obi.netbits@outlook.com>
2016-06-21 13:36:17 +0000

    v5: add css stylesheet

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var seedDB = require("./seeds");
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 
