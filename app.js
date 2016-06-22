967eb81c9fb0d77a60c3d263d73b2f6edbec398b
Obi <obi.netbits@outlook.com>
2016-06-22 11:04:26 +0000

    v6: install required auth packages, define user model

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var express = require("express");
-var app = express();
-var bodyParser= require("body-parser");
-var mongoose = require("mongoose");
-var Campground = require("./models/campground");
-var Comment = require("./models/comment");
-var seedDB = require("./seeds");
+var express       = require("express"),
+    app           = express(),
+    bodyParser    = require("body-parser"),
+    mongoose      = require("mongoose"),
+    passport      = require("passport"),
+    LocalStrategy = require("passport-local"),
+    Campground    = require("./models/campground"),
+    Comment       = require("./models/comment"),
+    User          = require("./models/user"),
+    seedDB        = require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
