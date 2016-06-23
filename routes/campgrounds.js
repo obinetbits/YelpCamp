7f044d419ec0f552f6d58ca09a073e079bdc140f
Obi <obi.netbits@outlook.com>
2016-06-23 22:20:16 +0000

    v10: add delete routes for campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -83,6 +83,17 @@ router.get("/:id", function(req, res){
     });
  });
  
+ //DESTROY CAMPGROUND ROUTE
+ router.delete("/:id", function(req, res){
+     Campground.findByIdAndRemove(req.params.id, function(err){
+         if(err){
+             res.redirect("/campgrounds");
+         } else {
+             res.redirect("/campgrounds");
+         }
+     });
+ });
+ 
  //middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
