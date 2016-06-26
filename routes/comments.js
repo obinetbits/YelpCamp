75a7c223712a12f666e837f25405d048ee4d05d8
Obi <obi.netbits@outlook.com>
2016-06-26 03:27:10 +0000

    add comment destroy route and button

--- routes/comments.js
+++ routes/comments.js
@@ -72,6 +72,18 @@ router.put("/:comment_id", function(req, res){
     })
 });
 
+//COOMENT DESTROY ROUTE
+router.delete("/:comment_id", function(req, res){
+    //findByIdAndRemove
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
 //middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
